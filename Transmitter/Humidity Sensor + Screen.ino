3e0185a7ec537a25b0d3c4790db6a754cb0942f0
BytePointer*0x2a <48522069+BytePointer0x2a@users.noreply.github.com>
2019-05-23 12:58:48 +0200

    Add files via upload

---
+++ Transmitter/Humidity Sensor + Screen.ino
@@ -0,0 +1,13 @@
+int humidity_sensor = A0;
+int humidity_value;
+
+void setup(){
+	Serial.begin(9600);
+	Serial.println("|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#");
+	Serial.println("SUPER COOL CONTROLLED WATER PUMP HAS STARTED");
+	Serial.println("|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#");
+
+
+
+
+}
